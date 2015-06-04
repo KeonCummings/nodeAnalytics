5ff76b047b318ff66617f86e46ce1e64f444f217
Keon Cummings <kcummings@kcummings.local>
2015-06-04 14:05:06 -0500

    using http service to get FB data

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,28 @@
+var app = angular.module('nodeAnalytics', []);
+
+app.factory('myService', function($http) {
+  var myService = {
+    async: function() {
+      // $http returns a promise, which has a then function, which also returns a promise
+      var promise = $http.get('https://graph.facebook.com/lowes').then(function (response) {
+        // The then function here is an opportunity to modify the response
+        console.log(response);
+        // The return value gets picked up by the then in the controller.
+        return response.data;
+      });
+      // Return the promise to the controller
+      return promise;
+    }
+  };
+  return myService;
+});
+
+
+app.controller('MainCtrl', [
+'$scope', 'myService',
+  function($scope, myService){
+    $scope.test = 'Hello world!';
+     myService.async().then(function(d) {
+      $scope.data = d.checkins;
+    });
+}]);
